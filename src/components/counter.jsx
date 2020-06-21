20739cb0e50b2dcd3a8fbac1251c87d711f95d73
Suhail M <suhailphp@gmail.com>
2020-06-21 21:19:08 +0400

    Rendering list of items in react

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,19 +2,29 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
+    tags: ["first item", "second item", "third item"],
   };
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count == 0 ? "warning" : "primary";
     return (
       <React.Fragment>
-        <span style={{ fontSize: 15 }} class={classes}>
+        <span style={{ fontSize: 15 }} class={this.getBadgeClass()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
+  }
+
+  getBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count == 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
