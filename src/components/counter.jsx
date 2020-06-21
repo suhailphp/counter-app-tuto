29e0df5b2a22d2c98e6adc31d9ce76bc376fb008
Suhail M <suhailphp@gmail.com>
2020-06-21 19:21:12 +0400

    components page updated with

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {};
+  state = {
+    count: 1,
+  };
   render() {
-    return <h1>Hello Dear</h1>;
+    let classes = "badge m-2 badge-";
+    classes += this.state.count == 0 ? "warning" : "primary";
+    return (
+      <React.Fragment>
+        <span style={{ fontSize: 15 }} class={classes}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
