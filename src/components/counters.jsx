c80621cfe4cf21c5f09219612e6aa0b730a71066
Suhail M <suhailphp@gmail.com>
2020-06-24 22:17:09 +0400

    Adding Reset Button and Reset all Counter values to Zero

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -21,9 +21,22 @@ class Counters extends Component {
     counters[index].value++;
     this.setState({ counters });
   };
+  handleReset = () => {
+    let counters = this.state.counters.map((counter) => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters });
+  };
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
